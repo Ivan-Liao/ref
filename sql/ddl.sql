6b42807035893b8317bd29d03453a6946d205fd8
ivanhliao <ivanhliao@gmail.com>
2025-08-03 17:55:40 -0400

    sql examples, some general some functions

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,5 +1,22 @@
+CREATE database my_database;
+CREATE schema my_schema;
+
+USE my_database;
+use my_schema; 
+
 CREATE TABLE authors (
 	author_name varchar(60),
 	author_email varchar(70),
 	author_pay int
+)
+
+-- example 2
+USE playground;
+CREATE TABLE bookings (
+	id int,
+    guest_id int,
+    listing_id int,
+    date_check_in date,
+    date_check_out date,
+    ds_book text
 )
---
+++ sql/general_2.sql
@@ -0,0 +1,38 @@
+-- CAST
+-- syntax
+SELECT CAST(<column_name> as <data_type>)
+-- example
+SELECT CAST(order_id as string)
+
+-- CONCAT
+-- syntax
+SELECT CONCAT(<column_name>, <text_string>, <column_name_2>)
+-- example
+SELECT CONCAT(name, ' ', id)
+
+-- DATEDIFF
+-- syntax
+DATEDIFF(<date_column_a>, <date_column_b>)
+-- example
+select listing_id,
+    sum(case when DATEDIFF(current_date(),date_check_in) < 90 then 1 else 0 end) as num_bookings_last90d,
+    sum(case when DATEDIFF(current_date(),date_check_in) between 91 and 365 then 1 else 0 end) as num_bookings_last365d,
+    count(date_check_in) as num_bookings_total
+from testcases.bookings_309_tc_1
+group by 1
+
+-- ROW_NUMBER()
+-- syntax
+ROW_NUMBER() OVER (PARTITION BY <column_a>, <column_b>, <column_x> ORDER BY <column_c>, <column_d>, <column_x>)
+-- example
+WITH summary AS (
+  SELECT a.genre, 
+           a.author_name, 
+           a.author_pay, 
+           ROW_NUMBER() OVER(PARTITION BY a.genre
+                                 ORDER BY a.author_pay DESC) AS rownum
+  FROM authors as a
+)
+SELECT s.*
+FROM summary as s
+WHERE s.rownum = 1;
