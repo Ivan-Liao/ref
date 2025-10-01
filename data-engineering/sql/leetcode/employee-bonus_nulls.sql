8b252a1666465701ed1ff97e08654845fe6aa5a1
ivanhliao <ivanhliao@gmail.com>
2025-09-30 23:31:39 -0400

    sql practice

---
+++ data-engineering/sql/leetcode/employee-bonus_nulls.sql
@@ -0,0 +1,8 @@
+-- missed solution on first pass due to not considering nulls
+select e.name,
+    b.bonus
+from Employee e
+left join Bonus b
+    on e.empId = b.empId
+where b.bonus < 1000
+    or b.bonus is null
